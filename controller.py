875a13ace8306c1e9d3083e9788e55ddc937d4e1
LuanSartori <luansartoridev@gmail.com>
2022-07-25 09:44:08 -0300

    Atualização na CategoriaDal / EstoqueDal + CategoriaDal -> ler_aquivo() / pesquisar_arquivo() - CategoriaDal -> ler_categoria() + EstoqueDal -> ler_arquivo() / pesquisar_arquivo() - EstoqueDal -> gerar_index() / ler_index

--- controller.py
+++ controller.py
@@ -17,49 +17,58 @@ class ServerError(Exception):
 class CategoriaController():
     @staticmethod
     def cadastrar(categoria: str):
-        if CategoriaDal.ler_arquivo('categoria', categoria):
+        codigo = CategoriaDal.ler_arquivo()
+        codigo_estoque = EstoqueDal.ler_arquivo()
+        if not codigo or not codigo_estoque:
+            raise ServerError('Não foi possível acessar o banco de dados!')
+
+        if CategoriaDal.pesquisar_arquivo(codigo, 'categoria', categoria):
             raise IdError(False, 'Já existe uma categoria com esse nome')
         if len(categoria) > 20:
             raise IdError(False, 'Esse nome é muito grande!')
         
-        codigo_estoque = EstoqueDal.ler_arquivo()
-        if not codigo_estoque:
-            raise ServerError('Não foi possível acessar o banco de dados!')
         
         id = CategoriaDal.gerar_id()
         categoria = Categoria(categoria, id)
-        return CategoriaDal.salvar(categoria, codigo_estoque)
+        return CategoriaDal.salvar(categoria, codigo, codigo_estoque)
 
 
     @staticmethod
     def alterar(id_categoria: int, categoria: str):
-        validacao = CategoriaDal.ler_arquivo('id', str(id_categoria) )
-        if not validacao:
-            raise IdError('Não existe uma categoria com esse ID', id_categoria)
-        i, codigo = validacao
+        codigo = CategoriaDal.ler_arquivo()
+        codigo_estoque = EstoqueDal.ler_arquivo()
+        if not codigo or not codigo_estoque:
+            raise ServerError('Não foi possível acessar o banco de dados!')
 
-        if CategoriaDal.ler_arquivo('categoria', categoria):
+        i = CategoriaDal.pesquisar_arquivo(codigo, 'id', str(id_categoria) )
+        if not i:
+            raise IdError('Não existe uma categoria com esse ID', id_categoria)
+
+        if CategoriaDal.pesquisar_arquivo(codigo, 'categoria', categoria):
             raise IdError(False, 'Já existe uma categoria com esse nome')
         if len(categoria) > 30:
             raise IdError(False, 'Esse nome é muito grande!')
 
-        codigo_estoque = EstoqueDal.ler_arquivo()
-        if not codigo_estoque:
-            raise ServerError('Não foi possível acessar o banco de dados!')
 
         categoria = Categoria(categoria, id_categoria)
-        return CategoriaDal.alterar(int(id_categoria), categoria, i, codigo, codigo_estoque)
+        return CategoriaDal.alterar(id_categoria, categoria, i, codigo, codigo_estoque)
 
 
     @staticmethod
     def remover(id_categoria: int):
-        validacao = CategoriaDal.ler_arquivo('id', str(id_categoria) )
-        if not validacao:
-            raise IdError('Não existe uma categoria com esse ID', id_categoria)
-        i, codigo = validacao
-        
+        codigo = CategoriaDal.ler_arquivo()
         codigo_estoque = EstoqueDal.ler_arquivo()
-        if not codigo_estoque:
+        if not codigo or not codigo_estoque:
             raise ServerError('Não foi possível acessar o banco de dados!')
+            
+        i = CategoriaDal.pesquisar_arquivo(codigo, 'id', str(id_categoria) )
+        if not i:
+            raise IdError('Não existe uma categoria com esse ID', id_categoria)
+        
 
         return CategoriaDal.remover(id_categoria, i, codigo, codigo_estoque)
+
+
+categoria = str(input('Nome da categoria: ')).lower()
+id = int(input('ID da categoria: '))
+CategoriaController.alterar(id, categoria)
--- dal.py
+++ dal.py
@@ -25,38 +25,33 @@ class CategoriaDal:
 
 
     @staticmethod
-    def ler_arquivo(chave: str, valor: str):
+    def ler_arquivo():
         try:
             with open('banco_dados/categorias.json', 'r') as arq:
-                codigo = json.load(arq)
-                for i, c in enumerate(codigo):
-                    if str( c[chave] ) == str(valor):
-                        return (i, codigo)
-                return False
+                return json.load(arq)
         except:
             return False
 
 
     @staticmethod
-    def ler_categoria(id: int) -> Categoria:
-        c = CategoriaDal.ler_arquivo('id', id)
-        
-        return Categoria(c['categoria'], c['id'])
+    def pesquisar_arquivo(codigo, chave: str, valor: str):
+        for i, c in enumerate(codigo):
+            if str( c[chave] ) == str(valor):
+                return i
+        return False
 
 
     @staticmethod
-    def salvar(categoria: Categoria, codigo_estoque) -> tuple:
+    def salvar(categoria: Categoria, codigo, codigo_estoque) -> tuple:
         try:
-            with open('banco_dados/categorias.json', 'r') as arq:
-                codigo = json.load(arq)
-                with open('banco_dados/categorias.json', 'w') as arq:
-                    nova_categoria = {'id': categoria.id, 'categoria': categoria.categoria}
-                    codigo.insert(categoria.id, nova_categoria)
-                    json.dump(codigo, arq, indent=4)
+            nova_categoria = {'id': categoria.id, 'categoria': categoria.categoria}
+            codigo.insert(categoria.id, nova_categoria)
 
-                    EstoqueDal.salvar_categoria(categoria, codigo_estoque)
+            with open('banco_dados/categorias.json', 'w') as arq:
+                json.dump(codigo, arq, indent=4)
+                EstoqueDal.salvar_categoria(categoria, codigo_estoque)
 
-                    return (True, 'Categoria cadastrada com sucesso!')
+                return (True, 'Categoria cadastrada com sucesso!')
         except:
             return (False, 'Erro interno do sistema')
 
@@ -67,7 +62,6 @@ class CategoriaDal:
             with open('banco_dados/categorias.json', 'w') as arq:
                 codigo[i] = {'id': alteracao.id, 'categoria': alteracao.categoria}
                 json.dump(codigo, arq, indent=4)
-
                 EstoqueDal.alterar_categoria(alteracao, codigo_estoque)
 
                 return (True, 'Categoria alterada com sucesso!')
@@ -85,10 +79,8 @@ class CategoriaDal:
             with open('banco_dados/ids.json', 'r') as arq:
                 codigo = json.load(arq)
                 codigo['id_categoria']['ids_vazios'].insert(0, id_categoria)
-                codigo['index_categoria']['index_vazios'].insert(0, id_categoria+1)
                 with open('banco_dados/ids.json', 'w') as arq:
                     json.dump(codigo, arq, indent=4)
-
                     EstoqueDal.remover_categoria(id_categoria, codigo_estoque)
 
                     return (True, 'Categoria removida com sucesso!')
@@ -103,52 +95,22 @@ class EstoqueDal:
     def ler_arquivo():
         try:
             with open('banco_dados/estoque.json', 'r') as arq:
-                codigo = json.load(arq)
-                return codigo
+                return json.load(arq)
         except:
-            return False
-
-
-    @staticmethod
-    def gerar_index():
-        with open('banco_dados/ids.json', 'r') as arq:
-            codigo = json.load(arq)
-            if len( codigo['index_categoria']['index_vazios'] ) == 0:
-                index = codigo['index_categoria']['novo_index']
-                codigo['index_categoria']['novo_index'] += 1
-            else:
-                index = codigo['index_categoria']['index_vazios'][0]
-                codigo['index_categoria']['index_vazios'].pop(0)
-
-            with open('banco_dados/ids.json', 'w') as arq:
-                json.dump(codigo, arq, indent=4)
-                return index
-
-
-    @staticmethod
-    def ler_index(id_categoria: int) -> int:
-        """
-        Retorna um index da lista a partir do id de uma categoria
-        """
-
-        with open('banco_dados/estoque.json', 'r') as arq:
-            codigo = json.load(arq)
-
-            for c in codigo[0]:
-                if c['id_categoria'] == id_categoria:
-                    index = c['index']
-                    return index
             return False
     
 
     @staticmethod
-    def salvar_categoria(categoria: Categoria, codigo: list):
-        index = EstoqueDal.gerar_index()
-        dado_1 = {"index": index, "id_categoria": categoria.id, "categoria": categoria.categoria}
-        dado_2 = {"categoria": categoria.categoria, "produtos": []}
+    def pesquisar_arquivo(codigo, id_categoria):
+        for i, c in enumerate(codigo):
+            if c['id'] == id_categoria:
+                return i
+    
 
-        codigo[0].insert(index-1, dado_1)
-        codigo.insert(index, dado_2)
+    @staticmethod
+    def salvar_categoria(categoria: Categoria, codigo: list):
+        dado = {"id": categoria.id, "categoria": categoria.categoria, "produtos": []}
+        codigo.insert(categoria.id, dado)
 
         with open('banco_dados/estoque.json', 'w') as arq:
             json.dump(codigo, arq, indent=4)
@@ -156,45 +118,37 @@ class EstoqueDal:
 
     @staticmethod
     def alterar_categoria(alteracao: Categoria, codigo: list):
-        index = EstoqueDal.ler_index(alteracao.id)
-        
-        for c in codigo[0]:
-            if c['index'] == index:
-                c['categoria'] = alteracao.categoria
+        index = EstoqueDal.pesquisar_arquivo(codigo, alteracao.id)
 
         codigo[index]['categoria'] = alteracao.categoria
-
         with open('banco_dados/estoque.json', 'w') as arq:
             json.dump(codigo, arq, indent=4)
     
 
     @staticmethod
     def remover_categoria(id_categoria: int, codigo: list):
-        index = EstoqueDal.ler_index(id_categoria)
-
+        index = EstoqueDal.pesquisar_arquivo(codigo, id_categoria)
+        
         for p in codigo[index]['produtos']:
             p['id_categoria'] = 0
-            codigo[1]['produtos'].append(p)
-        
-        for i, c in enumerate(codigo[0]):
-            if c['index'] == index:
-                codigo[0].pop(i)
+            codigo[0]['produtos'].append(p)
         codigo.pop(index)
+
         with open('banco_dados/estoque.json', 'w') as arq:
             json.dump(codigo, arq, indent=4)
      
 
     @staticmethod
-    def ler_produto(index_categoria: int, id_produto: int, codigo: list) -> Produto:
-        with open('banco_dados/estoque.json', 'r') as arq:
-            produtos = codigo[index_categoria]['produtos']
-            
-            for p in produtos:
-                if p['id'] == id_produto:
-                    return Produto(p['id'],
-                                   p['id_categoria'],
-                                   p['nome'],
-                                   p['marca'],
-                                   p['preco'],
-                                   p['id_fornecedor'])
-            return False
+    def ler_produto(id_categoria: int, id_produto: int, codigo: list) -> Produto:
+        index = EstoqueDal.pesquisar_arquivo(codigo, id_categoria)
+        produtos = codigo[index]['produtos']
+        
+        for p in produtos:
+            if p['id'] == id_produto:
+                return Produto(p['id'],
+                                p['id_categoria'],
+                                p['nome'],
+                                p['marca'],
+                                p['preco'],
+                                p['id_fornecedor'])
+        return False
